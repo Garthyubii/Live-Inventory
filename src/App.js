86b20e0dd31afe7b39ecabc470d0ee8bd0a5784e
Garthyubii <aldiofajar86@gmail.com>
2025-07-12 15:58:07 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>📦 Live Inventory</h1>
+      <p>Selamat datang di aplikasi penyimpanan realtime!</p>
+    </div>
+  );
+}
+
+export default App;
